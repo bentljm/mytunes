da4a46cf7dd8939bd654e03db6622524146aa9fc
Jeff Bentler <bentljm@gmail.com>
2017-01-04 19:58:09 -0800

    Completed SongQueueView

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,17 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+
   initialize: function() {
+    this.collection.on('add remove', this.render, this);
+
   },
 
   render: function() {
-    return this.$el;
+    var songQueue = new SongQueueEntryView({model: SongModel});
+    return this.$el.append(songQueue.render());
+
   }
 
 });
+
