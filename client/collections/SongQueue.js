51468ff7fdd00209f70d8164df47db42d5f613b7
Jeff Bentler <bentljm@gmail.com>
2017-01-04 19:32:29 -0800

    Completed SongModel and SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,43 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    //when a song is added
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+
+    //when a song ends
+    this.on('ended', function() {
+    //remove the song
+      this.remove(this.at(0));
+      //check to see if there are any songs left
+      if (this.length > 0) {
+        //and if there are, play the next one
+        this.playFirst();
+      }
+    }, this);
+
+
+
+    //when a song is dequeued
+    this.on('dequeue', function(song) {
+      //remove the song
+      this.remove(song);
+    }, this);
+
+  },
+
+  //play the first one in the queue
+  playFirst: function() {
+    this.at(0).play();
+
   }
 
 });
+
+
+
+
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,24 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  //has an play method that triggers an "play" event
   play: function() {
-    // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  //has an enqueue method that triggers an "enqueue" event
+  enqueue: function() {
+    this.trigger('enqueue', this);
+  },
+
+  //has a dequeue method that triggers a "dequeue" event
+  dequeue: function() {
+    this.trigger('dequeue', this);
+  },
+
+  //has an ended method that triggers an "ended" event
+  ended: function() {
+    this.trigger('ended', this);
   }
 
 });
